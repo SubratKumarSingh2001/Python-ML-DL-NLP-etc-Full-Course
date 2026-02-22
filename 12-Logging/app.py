ba107f5dead3c7047cc52cac0db410bd19ef0ece
Subrat Kumar Singh <92250219+SubratKumarSingh2001@users.noreply.github.com>
2026-02-22 17:07:34 +0530

    Add files via upload

---
+++ 12-Logging/app.py
@@ -0,0 +1,9 @@
+import logging
+
+#logging setting this basic configure will be applied to all the loggers created
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s-%(name)s-%(levelname)s-%(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    force=True
+)
